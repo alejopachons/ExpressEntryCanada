ff4694a1e86ac5ff4514ce49cb072b5e8d1582b2
alejopachons <mapachonpa@gmail.com>
2025-05-01 19:56:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ for tipo in tipos_unicos:
 tipos_seleccionados = [tipo for tipo, seleccionado in selecciones.items() if seleccionado]
 df_filtrado = df[df["Tipo de Ronda"].isin(tipos_seleccionados)]
 
-st.title("Invitaciones Express Entry (Canadá) 🇨🇦 ")
+st.title("Invitaciones Express Entry (Canadá)")
 
 # Gráfico 1: Invitaciones por fecha
 fig1 = px.line(df_filtrado, x="Fecha", y="Invitaciones", color="Tipo de Ronda",
