fdb3480913c9fdc3e82284e73b299bd4e030f299
alejopachons <mapachonpa@gmail.com>
2025-05-04 13:33:26 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-st.set_page_config(layout="wide")
+st.set_page_config(layout="wide", page_title="Express Entry")
 
 # Cargar datos
 df = pd.read_csv("Canada.csv", sep=";")
