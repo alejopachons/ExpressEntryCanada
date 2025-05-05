d52f01388194058cf1debff8d8606fa94f3b50f6
alejopachons <mapachonpa@gmail.com>
2025-05-05 18:05:12 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
+st.set_page_config(layout="wide", page_title="Canada calc")
+
 # Sidebar para navegación
 st.sidebar.title("Navegación")
 seleccion = st.sidebar.radio("Ir a:", ["Express Entry", "MPNP"])
