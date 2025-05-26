5eb6c98d9c90a808bab28a74d4aaac4307dd4ccc
mapachonpa@gmail.com <Alejandro>
2025-05-26 15:48:10 -0500

    updated

--- draws.py
+++ draws.py
@@ -2,23 +2,22 @@ import streamlit as st
 
 def run():
 
-    st.set_page_config(layout="wide")
     st.title("Draws")
 
-    # col1, col2 = st.columns(2)
+    col1, col2 = st.columns(2)
 
-    # with col1:
-    #     st.subheader("Express Entry – Canadá")
-    #     st.components.v1.iframe(
-    #         "https://www.canada.ca/en/immigration-refugees-citizenship/corporate/mandate/policies-operational-instructions-agreements/ministerial-instructions/express-entry-rounds.html",
-    #         height=700,
-    #         scrolling=True
-    #     )
+    with col1:
+        st.subheader("Express Entry – Canadá")
+        st.components.v1.iframe(
+            "https://www.canada.ca/en/immigration-refugees-citizenship/corporate/mandate/policies-operational-instructions-agreements/ministerial-instructions/express-entry-rounds.html",
+            height=700,
+            scrolling=True
+        )
 
-    # with col2:
-    #     st.subheader("EOI Draws – Manitoba")
-    #     st.components.v1.iframe(
-    #         "https://immigratemanitoba.com/notices/eoi-draw/",
-    #         height=700,
-    #         scrolling=True
-    #     )
+    with col2:
+        st.subheader("EOI Draws – Manitoba")
+        st.components.v1.iframe(
+            "https://immigratemanitoba.com/notices/eoi-draw/",
+            height=700,
+            scrolling=True
+        )
